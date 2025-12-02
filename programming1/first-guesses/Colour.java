d1b06f1d9525b6240304b93b77d1324b1973f7e5
byte-id <idama0513@gmail.com>
2025-12-02 16:27:26 +1100

    Delete programming1/first-guesses directory

--- programming1/first-guesses/Colour.java
+++
@@ -1,3 +0,0 @@
-public enum Colour {
-    RED, GREEN, YELLOW, BLUE;
-}
--- programming1/first-guesses/Runner.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-
-public class Runner{
-    public static void main(String[] args){
-        // Use me for testing!
-        System.out.print("Hi! Welcome to Sleuth!");
-    }
-}
